a1bedd9e268754c770583b200709ad897e2df85c
Iliess <m.iliess.abdelmadjid@gmail.com>
2024-07-12 22:32:50 +0200

    update the mvc exercices

--- the_mountain/mvc/Starter-pack/Model/connect_db.php
+++ the_mountain/mvc/Starter-pack/Model/connect_db.php
@@ -18,7 +18,9 @@ try {
     // Set error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+
     echo "Connected successfully";
+    
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
