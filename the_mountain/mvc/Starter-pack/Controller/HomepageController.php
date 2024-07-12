2ee3fc45c18cc6158d9a3be41133f8c83e6a1d87
Iliess <m.iliess.abdelmadjid@gmail.com>
2024-07-12 13:40:59 +0200

    update

---
+++ the_mountain/mvc/Starter-pack/Controller/HomepageController.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types = 1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+class HomepageController
+{
+    public function index()
+    {
+        // Usually, any required data is prepared here
+        // For the home, we don't need to load anything
+
+        // Load the view
+        require 'View/home.php';
+    }
+}
---
+++ the_mountain/mvc/Starter-pack/Model/connect_db.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+$servername = "localhost:3306";
+$username = "root";
+$password = "password";
+$dbname = "mvc_exercice";
+
+try {
+    // Create a PDO connection
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    // Set error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
